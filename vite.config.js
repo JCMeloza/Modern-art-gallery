d55dc616bca5993672aec292fe74e6ebd42fab47
JuanCarlos <melozajuancarlos@gmail.com>
2025-05-22 12:50:59 +0200

    Agregar configuración de construcción en vite.config.js para definir entradas de archivos HTML.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: "/Modern-art-gallery/",
   server: {
     watch: {
       usePolling: true
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        location: resolve(__dirname, 'location.html')
+      }
     }
   }
 })
